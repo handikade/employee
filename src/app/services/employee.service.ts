62260767dbc77181a4c225cef83ad6e6fe47df3c
Dika Handika <dika.handika@paper.id>
2021-04-29 08:46:23 +0700

    getEmployeeByUsername()

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { Observable } from 'rxjs';
+import { Observable, Subscriber } from 'rxjs';
+import employeeJson from '@app/shared/mocks/employees.json';
+
+import { Employee } from '@app/shared/types/Employee';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +13,25 @@ export class EmployeeService {
 
   getEmployees(): Observable<any> {
     return this.http.get('assets/employees.json');
+  }
+
+  getEmployeeByUsername(username: string): Observable<any> {
+    return new Observable((subscriber: Subscriber<any>) => {
+      const employees: Employee[] = employeeJson;
+      const found: Employee | undefined = employees.find((employee: Employee) => employee.username === username);
+
+      if (found) {
+        subscriber.next({
+          status: 200,
+          employee: found
+        });
+        subscriber.complete();
+      } else {
+        subscriber.error({
+          status: 404,
+          message: 'Employee not found'
+        });
+      }
+    });
   }
 }
